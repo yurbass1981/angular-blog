6c2db8707c85ae72f865f974da1e33b5f4b6ee25
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-06 13:59:52 +0300

    Created auth service

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { User } from "../interfaces";
+import { Observable, tap } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable()
+export class AuthService {
+    constructor(private http: HttpClient) {}
+
+    get token(): String {
+        return ''
+    }
+
+    login(user: User): Observable<any> {
+        return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+        .pipe(
+            tap(this.setToken)
+        )
+    }
+
+    logout() {
+
+    }
+
+    isAuthenticated(): boolean {
+        return !!this.token
+    }
+
+    private setToken(response: any) {
+        console.log(response)
+    }
+}
