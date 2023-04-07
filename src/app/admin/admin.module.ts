0dce49f620c472474c8f4b2aab01f586d9f8ba0e
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-07 18:03:34 +0300

    Corrected code

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,7 +8,7 @@ import { LoginPageComponent } from "./login-page/login-page.component";
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { CreatePageComponent } from './create-page/create-page.component';
 import { EditPageComponent } from './edit-page/edit-page.component';
-import { AuthService } from "../shared/services/auth.service";
+import { AuthService } from "./shared/services/auth.service";
 import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
