f8b3bff25a75262a0b8fbda95ccb17bccb9fb52a
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-07 19:15:19 +0300

    Corrected code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 import { Environment } from "./interface";
 
-export const environment: Environment {
+export const environment: Environment = {
     prodaction: true, 
     apiKey: 'AIzaSyCJEJLBanFHX3sSMSnLVLAMd7o6ldNfQlA'
-}
+};
