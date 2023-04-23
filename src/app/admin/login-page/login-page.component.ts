889ce5dc4579736782b4e18c4d70b081b6fc701b
Yuri Ilyushin <yurbass1@rambler.ru>
2023-04-23 11:14:07 +0300

    Added submitted

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -11,7 +11,8 @@ import { AuthService } from 'src/app/admin/shared/services/auth.service';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-  form: FormGroup;
+  form: FormGroup
+  submitted = false
 
   constructor(
     private auth: AuthService,
@@ -35,7 +36,9 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
-    const user: User | any = {
+    this.submitted = true 
+
+    const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
     }
@@ -43,6 +46,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset()
       this.router.navigate(['/admin', 'dashboard'])
+      this.submitted = false
     })
   }
 }
